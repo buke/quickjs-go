ed45cfe48ff05b7bd3ba56bb066853039bf8480e
wangbuke <wangbuke@gmail.com>
2022-06-10 17:50:04 +0800

    windows support

--- quickjs.go
+++ quickjs.go
@@ -9,6 +9,7 @@ package quickjs
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/libs/darwin_arm64 -lquickjs -lm
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/linux_amd64 -lquickjs -lm
 #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/deps/libs/linux_arm64 -lquickjs -lm
-
+#cgo windows,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/windows_amd64 -lquickjs -lm
+#cgo windows,386 LDFLAGS: -L${SRCDIR}/deps/libs/windows_386 -lquickjs -lm
 */
 import "C"
