cc7622ba58730590999a7e6b3d58fa69b2a5bb0d
wangbuke <wangbuke@gmail.com>
2022-07-29 22:00:07 +0800

    code clean

--- loop.go
+++ loop.go
@@ -1,16 +1,5 @@
 package quickjs
 
-import (
-	"fmt"
-	"runtime"
-	"strconv"
-	"strings"
-	"sync"
-)
-
-// https://github.com/robertkrimen/natto/blob/5296a7476556988e191b1f0f739bd2e001273d56/natto.go
-// https://github.com/dop251/goja_nodejs/blob/master/eventloop/eventloop_test.go
-
 type Job func()
 
 type Loop struct {
@@ -33,8 +22,6 @@ func (l *Loop) ScheduleJob(j Job) error {
 func (l *Loop) IsLoopPending() bool {
 	return len(l.jobChan) > 0
 }
-
-var loopLock sync.Mutex
 
 // run executes all pending jobs.
 func (l *Loop) Run() error {
@@ -61,15 +48,4 @@ func (l *Loop) Run() error {
 func (l *Loop) Stop() error {
 	close(l.jobChan)
 	return nil
-}
-
-func Goid() int {
-	var buf [64]byte
-	n := runtime.Stack(buf[:], false)
-	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
-	id, err := strconv.Atoi(idField)
-	if err != nil {
-		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
-	}
-	return id
 }
