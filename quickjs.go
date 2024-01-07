3b702852211438cdfa18b4c6da76478bc94077ba
wangbuke <wangbuke@gmail.com>
2024-01-07 11:22:05 +0800

    using original quickjs.h

--- quickjs.go
+++ quickjs.go
@@ -4,7 +4,7 @@ Package quickjs Go bindings to QuickJS: a fast, small, and embeddable ES2020 Jav
 package quickjs
 
 /*
-#cgo CFLAGS: -I./deps/include
+#cgo CFLAGS: -I./deps/quickjs
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/darwin_amd64 -lquickjs -lm
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/libs/darwin_arm64 -lquickjs -lm
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/linux_amd64 -lquickjs -lm
